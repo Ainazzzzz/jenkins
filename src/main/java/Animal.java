c7cd73c0c55505dfd17ac7e07c8f8b830c3874f1
AinazZz <124769374+Ainazzzzz@users.noreply.github.com>
2023-10-18 21:38:43 +0600

    Update Animal.java

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -7,21 +7,7 @@ public class Animal {
         this.name = name;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+  
 
     @Override
     public String toString() {
